c3d8b2e4184254263b0426edd2be1b6daa0ff1fa
aparna khare <aparna707khare@gmail.com>
2021-10-08 12:18:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT ||5000;
+
+
+//set handlebars middlebare
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there,this is other stuff!";
+//set handlebars routes
+app.get('/', function (req, res) {
+    res.render('home',{
+    	stuff: otherstuff
+    });
+});
+
+
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+
+
+
+app.listen(PORT,() => console.log('server listening on port ' + PORT));
